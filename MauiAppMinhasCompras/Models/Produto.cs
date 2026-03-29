ea56bcbd39ea294ef8ea28c46b655a5ad95400fa
User <User@DESKTOP-UG9ES2L>
2026-03-29 16:17:37 -0300

    Construção da classe para fazer Crud no SQLite

--- MauiAppMinhasCompras/Models/Produto.cs
+++ MauiAppMinhasCompras/Models/Produto.cs
@@ -1,12 +1,13 @@
 ﻿using SQLite;
+
 namespace MauiAppMinhasCompras.Models
 {
     public class Produto
     {
-        [ PrimaryKey, AutoIncrement]
-        public int Id { get; set; }  
-        public string Descricao {  get; set; }
+        [PrimaryKey, AutoIncrement]
+        public int Id { get; set; }
+        public string Descricao { get; set; }
         public double Quantidade { get; set; }
-        public double Preco {  get; set; }
+        public double Preco { get; set; }
     }
 }
